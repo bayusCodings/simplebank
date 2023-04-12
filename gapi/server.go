3daac98e04e08d52d0b5b6280450ba880315acd7
Abayomi <abayomi@Abayomis-MacBook-Pro-2.local>
2023-04-12 12:10:09 +0100

    task queueing

--- gapi/server.go
+++ gapi/server.go
@@ -7,26 +7,29 @@ import (
 	"github.com/bayuscodings/simplebank/pb"
 	"github.com/bayuscodings/simplebank/token"
 	"github.com/bayuscodings/simplebank/util"
+	"github.com/bayuscodings/simplebank/worker"
 )
 
 type Server struct {
 	pb.UnimplementedSimpleBankServer
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
 // NewServer creates a new gRPC server
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(config util.Config, store db.Store, taskDistributor worker.TaskDistributor) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
 	}
 
 	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
